0882ef20245ed5fda130524a6291de483cc3df46
C0921G1-quockhanh <ledinhquockhanhdn97@gmail.com>
2022-01-12 12:08:15 +0700

    upbai

---
+++ ss2_spring_mvc_controller/thuc_hanh/email_validate_gradle/src/main/java/qk/email_validate_gradle/HomeController.java
@@ -0,0 +1,45 @@
+package qk.email_validate_gradle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Controller
+public class HomeController {
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
+    private static Pattern pattern;
+    private Matcher matcher;
+
+    public HomeController() {
+        pattern = Pattern.compile(EMAIL_REGEX);
+    }
+
+    @GetMapping(value = "/")
+    public String home() {
+        return "home";
+    }
+
+    @PostMapping(value = "/validate")
+    public String user(@RequestParam(value = "email") String inputEmail, ModelMap modelMap) {
+        boolean isValid = this.validate(inputEmail);
+
+        if (isValid) {
+            modelMap.addAttribute("email",inputEmail);
+            return "success";
+        } else {
+            modelMap.addAttribute("errorEmail",inputEmail);
+            modelMap.addAttribute("message","Email is invalid");
+            return "home";
+        }
+    }
+
+    private boolean validate(String regex) {
+        matcher = pattern.matcher(regex);
+        return matcher.matches();
+    }
+}
