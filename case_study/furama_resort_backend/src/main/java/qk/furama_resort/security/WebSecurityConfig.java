4ac5e2463b8f62bf7466025fb42240f841d3c86c
C0921G1-quockhanh <ledinhquockhanhdn97@gmail.com>
2022-02-23 17:13:51 +0700

    upbai

--- case_study/furama_resort_backend/src/main/java/qk/furama_resort/security/WebSecurityConfig.java
+++ case_study/furama_resort_backend/src/main/java/qk/furama_resort/security/WebSecurityConfig.java
@@ -34,7 +34,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .formLogin()
-                .loginPage("/login")
+//                .loginPage("/login")
                 .defaultSuccessUrl("/").permitAll()
                 .and()
                 .authorizeRequests()
